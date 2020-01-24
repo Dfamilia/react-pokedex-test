3bd4c461ba0dfcece9ffb2632e93bad705646376
dfamilia <dfamilia7@gmail.com>
2020-01-24 15:09:42 -0400

    project init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+const App = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="jumbotron">hola mundo</div>
+
+            </div>
+            Restaurantes
+        </div>
+    );
+};
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
